939547fb665f93fb8a4838183c1ad048d70f2a42
r0ckbiter <insanerealty@gmail.com>
2023-01-25 21:05:15 +0200

    updating lock file

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
-
-import argparse
 import os
+import sys
+import argparse
 
+import page_loader.page_downloader
 from page_loader.page_downloader import download
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Page-loader')
-    parser.add_argument('url_address')
-    parser.add_argument('-o', '--output', default=os.getcwd())
-    args = parser.parse_args()
-    file_path = download(args.url_address, args.output)
-    print(f'Downloading complete. Path to file: {file_path}')
+    try:
+        parser = argparse.ArgumentParser(description='Page-loader')
+        parser.add_argument('url_address')
+        parser.add_argument('-o', '--output', default=os.getcwd())
+        args = parser.parse_args()
+        page_path = download(args.url_address, args.output)
+        print(f'Downloading complete. Path to file: {page_path}')
+    except page_loader.page_downloader.AppInternalError as e:
+        print(e)
+        sys.exit(1)
+    else:
+        sys.exit(2)
 
 
 if __name__ == '__main__':
